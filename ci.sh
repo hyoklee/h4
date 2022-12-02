6b4f1b3541f5b3978bdc3069656eff97bcab32cc
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-01 22:10:51 -0600

    ci: remove rpc

--- ci.sh
+++ ci.sh
@@ -2,10 +2,11 @@
 echo $CFLAGS
 echo $LDFLAGS
 export CFLAGS+=" -I/usr/include/tirpc/"
-export LDFLAGS+=" -ltirpc -lxdr -lrpc"
+export LDFLAGS+=" -ltirpc -lxdr"
 echo $CFLAGS
 echo $LDFLAGS
 autoreconf -fvi
-./configure --disable-fortran --disable-netcdf 
+./configure --disable-fortran --disable-netcdf
+cat ./config.log
 make
 make test

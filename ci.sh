aaaa6ef9159d85785a2672ad413183a8635ffb3f
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-01 22:41:26 -0600

    ci: define EXT2LIB

--- ci.sh
+++ ci.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo $CFLAGS
 echo $LDFLAGS
+echo $EXT2LIB
 export CFLAGS+=" -I/usr/include/tirpc/"
-export LDFLAGS+=" -ltirpc -lxdr"
+export LDFLAGS+=" -ltirpc"
+export EXT2LIB+=" -ltirpc"
 echo $CFLAGS
 echo $LDFLAGS
+echo $EXT2LIB
 autoreconf -fvi
 ./configure --disable-fortran --disable-netcdf
 cat ./config.log

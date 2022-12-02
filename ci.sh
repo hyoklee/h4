1e73691d421fee8a1f1ed0246ff5767c3367979e
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-01 21:09:12 -0600

    ci: fix syntax

--- ci.sh
+++ ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo $CFLAGS
 echo $LDFLAGS
-export CFLAGS += -I/usr/include/tirpc/
-export LDFLAGS += -ltirpc
+export CFLAGS+=" -I/usr/include/tirpc/"
+export LDFLAGS+=" -ltirpc"
 echo $CFLAGS
 echo $LDFLAGS
 autoreconf -fvi

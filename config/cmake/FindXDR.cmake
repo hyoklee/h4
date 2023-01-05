efda77d3cdd707f4617774d138a3b2ca75d94e63
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2023-01-05 15:08:45 -0600

    fix: search drpc

--- config/cmake/FindXDR.cmake
+++ config/cmake/FindXDR.cmake
@@ -14,14 +14,14 @@ include(CheckTypeSize)
 include(CheckLibraryExists)
 
 ## First try to find the required header files (rpc/types.h, rpc/xdr.h)
-find_path(XDR_INCLUDE_DIR NAMES rpc/types.h PATHS "/usr/include" "/usr/include/tirpc")
+find_path(XDR_INCLUDE_DIR NAMES rpc/types.h PATHS "/usr/include" "/usr/include/tirpc" "/usr/local/include/drpc")
 
 #check for the XDR functions: their interface and the libraries they're hidden in.
     ## Now let's see if we need an extra lib to compile it
     set(XDR_INT_FOUND)
     CHECK_FUNCTION_EXISTS(xdr_int XDR_INT_FOUND)
     if (NOT XDR_INT_FOUND)
-        foreach(lib nsl rpc tirpc)
+        foreach(lib nsl rpc tirpc drpc)
             ## Try to find the corresponding lib
             message(STATUS "Try to find the library ${lib}.")
             set(XDR_INT_LIBRARY)
